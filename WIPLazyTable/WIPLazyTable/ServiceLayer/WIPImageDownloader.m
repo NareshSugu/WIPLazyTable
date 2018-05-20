085a831ae5c823045ab30c6f3edd2dfffc6c28ee
Sugu <kumar.sugu.naresh@gmail.com>
2018-05-21 04:17:20 +0530

    [iOS][IMAGEDOWNLOAD] - defining execution of image download task

--- WIPLazyTable/WIPLazyTable/ServiceLayer/WIPImageDownloader.m
+++ WIPLazyTable/WIPLazyTable/ServiceLayer/WIPImageDownloader.m
@@ -8,6 +8,7 @@
 
 #import "WIPImageDownloader.h"
 #import "WIPCountryBioGraphyRowsContent.h"
+#import "WIPAppCommon.h"
 
 @interface WIPImageDownloader ()
 
@@ -19,12 +20,59 @@
 
 - (void)startDownload
 {
-    //
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:[self rowContentRecord].tileImageURLString]];
+    
+    _sessionTask = [[NSURLSession sharedSession] dataTaskWithRequest:request
+                                                   completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                                                       
+                                                       if (error != nil)
+                                                       {
+                                                           if (@available(iOS 9.0, *)) {
+                                                               if ([error code] == NSURLErrorAppTransportSecurityRequiresSecureConnection)
+                                                               {
+                                                                   // if you get error NSURLErrorAppTransportSecurityRequiresSecureConnection (-1022),
+                                                                   // then your Info.plist has not been properly configured to match the target server.
+                                                                   //
+                                                                   abort();
+                                                               }
+                                                           } else {
+                                                               // Fallback on earlier versions
+                                                           }
+                                                       }
+                                                       
+                                                       [[NSOperationQueue mainQueue] addOperationWithBlock: ^{
+                                                           
+                                                           // Set appIcon and clear temporary data/image
+                                                           UIImage *image = [[UIImage alloc] initWithData:data];
+                                                           
+                                                           if (image.size.width != APP_COMMON.standardCellImageViewWidth || image.size.height != APP_COMMON.standardCellImageViewHeight)
+                                                           {
+                                                               CGSize itemSize = CGSizeMake(APP_COMMON.standardCellImageViewWidth, APP_COMMON.standardCellImageViewHeight);
+                                                               UIGraphicsBeginImageContextWithOptions(itemSize, NO, 0.0f);
+                                                               CGRect imageRect = CGRectMake(0.0, 0.0, itemSize.width, itemSize.height);
+                                                               [image drawInRect:imageRect];
+                                                               [self rowContentRecord].tileImage = UIGraphicsGetImageFromCurrentImageContext();
+                                                               UIGraphicsEndImageContext();
+                                                           }
+                                                           else
+                                                           {
+                                                               [self rowContentRecord].tileImage = image;
+                                                           }
+                                                           
+                                                           if (self.completionHandler != nil)
+                                                           {
+                                                               self.completionHandler();
+                                                           }
+                                                       }];
+                                                   }];
+    
+    [self.sessionTask resume];
 }
 
 - (void)cancelDownload
 {
-    //
+    [self.sessionTask cancel];
+    _sessionTask = nil;
 }
 
 @end
