5ec06f249933a9aa4866f9288d84258f8e133dc2
Sugu <kumar.sugu.naresh@gmail.com>
2018-05-20 14:45:00 +0530

    [iOS][APPCOMMON] - App common file created

---
+++ WIPLazyTable/WIPLazyTable/AppCommon/WIPAppCommon.h
@@ -0,0 +1,13 @@
+//
+//  WIPAppCommon.h
+//  WIPLazyTable
+//
+//  Created by Norace Kumar on 5/20/18.
+//  Copyright © 2018 WiproDigital. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WIPAppCommon : NSObject
+
+@end
---
+++ WIPLazyTable/WIPLazyTable/AppCommon/WIPAppCommon.m
@@ -0,0 +1,13 @@
+//
+//  WIPAppCommon.m
+//  WIPLazyTable
+//
+//  Created by Norace Kumar on 5/20/18.
+//  Copyright © 2018 WiproDigital. All rights reserved.
+//
+
+#import "WIPAppCommon.h"
+
+@implementation WIPAppCommon
+
+@end
