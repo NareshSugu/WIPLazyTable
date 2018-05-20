812d29a1a43acd153bc0e1f9e15efb116a0ca3d2
Sugu <kumar.sugu.naresh@gmail.com>
2018-05-20 23:14:22 +0530

    [iOS][PARSER] - able to read json data and logged - converting dispatch_data_t to nsdata - encoding issue fixed - logging response

--- WIPLazyTable/WIPLazyTable/Parser/WIPDataParser.m
+++ WIPLazyTable/WIPLazyTable/Parser/WIPDataParser.m
@@ -8,6 +8,13 @@
 
 #import "WIPDataParser.h"
 
+@interface WIPDataParser ()
+
+@property (nonatomic, strong) NSData *dataToParse;
+@property (nonatomic, strong) NSDictionary *CountryBioGraphyDict;
+
+@end
+
 @implementation WIPDataParser
 
 - (instancetype)initWithData:(NSData *)data
@@ -15,9 +22,28 @@
     self = [super init];
     if (self != nil)
     {
+        const void *buffer = NULL;
+        size_t size = 0;
+        dispatch_data_t new_data_file = dispatch_data_create_map(data, &buffer, &size);
+        if(new_data_file){ /* to avoid warning really - since dispatch_data_create_map demands we care about the return arg */}
         
+        _dataToParse = [[NSData alloc] initWithBytes:buffer length:size];
+
+        [self parseJsonData];
     }
     return self;
 }
 
+- (void)parseJsonData {
+    
+    NSError* error = nil;
+    NSString *strISOLatin = [[NSString alloc] initWithData:_dataToParse encoding:NSISOLatin1StringEncoding];
+    _dataToParse = nil;
+    NSData *dataUTF8 = [strISOLatin dataUsingEncoding:NSUTF8StringEncoding];
+
+    _countryBioGraphyList = [NSJSONSerialization JSONObjectWithData:dataUTF8 options:NSJSONReadingMutableContainers error:&error];
+    
+    NSLog(@"Response Json %@", [_countryBioGraphyList description]);
+    
+}
 @end
