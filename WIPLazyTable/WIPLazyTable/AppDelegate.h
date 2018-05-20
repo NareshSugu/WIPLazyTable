3e68737390dc043e1aebf58cf7a5d07662bba416
Sugu <kumar.sugu.naresh@gmail.com>
2018-05-20 13:52:12 +0530

    [iOS] - Initial commit - Default project template created.

---
+++ WIPLazyTable/WIPLazyTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WIPLazyTable
+//
+//  Created by Norace Kumar on 5/20/18.
+//  Copyright © 2018 WiproDigital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
