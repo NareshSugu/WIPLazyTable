685883485bd049a9d6a6373bceeeab28df6dbea7
Sugu <kumar.sugu.naresh@gmail.com>
2018-05-20 20:19:50 +0530

    [iOS][DATAPARSER] -  data parser class initial commit

---
+++ WIPLazyTable/WIPLazyTable/Parser/WIPDataParser.h
@@ -0,0 +1,13 @@
+//
+//  WIPDataParser.h
+//  WIPLazyTable
+//
+//  Created by Norace Kumar on 5/20/18.
+//  Copyright © 2018 WiproDigital. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WIPDataParser : NSObject
+
+@end
---
+++ WIPLazyTable/WIPLazyTable/Parser/WIPDataParser.m
@@ -0,0 +1,13 @@
+//
+//  WIPDataParser.m
+//  WIPLazyTable
+//
+//  Created by Norace Kumar on 5/20/18.
+//  Copyright © 2018 WiproDigital. All rights reserved.
+//
+
+#import "WIPDataParser.h"
+
+@implementation WIPDataParser
+
+@end
