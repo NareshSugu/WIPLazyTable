5d7eaa99accae9b0519485eeb1d6bea78f2eead6
Sugu <kumar.sugu.naresh@gmail.com>
2018-05-20 20:18:28 +0530

    [iOS][SERVICELAYER]- initial commit

---
+++ WIPLazyTable/WIPLazyTable/ServiceLayer/WIPServiceLayer.m
@@ -0,0 +1,13 @@
+//
+//  WIPServiceLayer.m
+//  WIPLazyTable
+//
+//  Created by Norace Kumar on 5/20/18.
+//  Copyright © 2018 WiproDigital. All rights reserved.
+//
+
+#import "WIPServiceLayer.h"
+
+@implementation WIPServiceLayer
+
+@end
