dc8474a48f74b1512e76b1001e913aee1bf397bb
Sugu <kumar.sugu.naresh@gmail.com>
2018-05-20 18:29:23 +0530

    [iOS][CONSTRAINTS] - code indentation

--- WIPLazyTable/WIPLazyTable/AutoLayout/UIView+Autolayout.m
+++ WIPLazyTable/WIPLazyTable/AutoLayout/UIView+Autolayout.m
@@ -19,10 +19,14 @@
 // Relative to Parent
 //-----------------------------------------------------
 
-- (NSLayoutConstraint* ) constraintAlignAttributeToParent:(NSLayoutAttribute)attribute constant:(CGFloat)constant
+- (NSLayoutConstraint* ) constraintAlignAttributeToParent:(NSLayoutAttribute)attribute
+                                                 constant:(CGFloat)constant
 {
     
-    NSLayoutConstraint *constraint = [NSLayoutConstraint constraintWithItem:self attribute:attribute relatedBy:NSLayoutRelationEqual toItem:self.superview attribute:attribute multiplier:1.0f constant:constant];
+    NSLayoutConstraint *constraint = [NSLayoutConstraint constraintWithItem:self
+                                                                  attribute:attribute relatedBy:NSLayoutRelationEqual toItem:self.superview
+                                                                  attribute:attribute
+                                                                 multiplier:1.0f constant:constant];
     [NSLayoutConstraint activateConstraints:@[constraint]];
     return constraint;
 }
@@ -35,18 +39,66 @@
               [self constraintAlignVerticallyInParent]];
 }
 
--(NSLayoutConstraint*) constraintAlignVerticallyInParent{return [self constraintAlignAttributeToParent:NSLayoutAttributeCenterY constant:0];}
--(NSLayoutConstraint*) constraintAlignHorizontallyInParent{return [self constraintAlignAttributeToParent:NSLayoutAttributeCenterX constant:0];}
--(NSLayoutConstraint*) constraintAlignTopInParent{return [self constraintAlignAttributeToParent:NSLayoutAttributeTop constant:0];}
--(NSLayoutConstraint*) constraintAlignBottomInParent{return [self constraintAlignAttributeToParent:NSLayoutAttributeBottom constant:0];}
--(NSLayoutConstraint*) constraintAlignWidthInParent{return [self constraintAlignAttributeToParent:NSLayoutAttributeWidth constant:0];}
--(NSLayoutConstraint*) constraintAlignHeightInParent{return [self constraintAlignAttributeToParent:NSLayoutAttributeHeight constant:0];}
--(NSLayoutConstraint*) constraintAlignLeadingInParent{return [self constraintAlignAttributeToParent:NSLayoutAttributeLeading constant:0];}
--(NSLayoutConstraint*) constraintAlignTrailingInParent{return [self constraintAlignAttributeToParent:NSLayoutAttributeTrailing constant:0];}
--(NSLayoutConstraint*) constraintAlignTopInParentWithDistance:(CGFloat)distance{return [self constraintAlignAttributeToParent:NSLayoutAttributeTop constant:distance];}
--(NSLayoutConstraint*) constraintAlignBottomInParentWithDistance:(CGFloat)distance{return [self constraintAlignAttributeToParent:NSLayoutAttributeBottom constant:-distance];}
--(NSLayoutConstraint*) constraintAlignLeadingInParentWithDistance:(CGFloat)distance{return [self constraintAlignAttributeToParent:NSLayoutAttributeLeading constant:distance];}
--(NSLayoutConstraint*) constraintAlignTrailingInParentWithDistance:(CGFloat)distance{return [self constraintAlignAttributeToParent:NSLayoutAttributeTrailing constant:-distance];}
+-(NSLayoutConstraint*) constraintAlignVerticallyInParent{
+    
+    return [self constraintAlignAttributeToParent:NSLayoutAttributeCenterY constant:0];
+    
+}
+-(NSLayoutConstraint*) constraintAlignHorizontallyInParent{
+    
+    return [self constraintAlignAttributeToParent:NSLayoutAttributeCenterX constant:0];
+    
+}
+-(NSLayoutConstraint*) constraintAlignTopInParent{
+    
+    return [self constraintAlignAttributeToParent:NSLayoutAttributeTop constant:0];
+    
+}
+-(NSLayoutConstraint*) constraintAlignBottomInParent{
+    
+    return [self constraintAlignAttributeToParent:NSLayoutAttributeBottom constant:0];
+    
+}
+-(NSLayoutConstraint*) constraintAlignWidthInParent{
+    
+    return [self constraintAlignAttributeToParent:NSLayoutAttributeWidth constant:0];
+    
+}
+-(NSLayoutConstraint*) constraintAlignHeightInParent{
+    
+    return [self constraintAlignAttributeToParent:NSLayoutAttributeHeight constant:0];
+    
+}
+-(NSLayoutConstraint*) constraintAlignLeadingInParent{
+    
+    return [self constraintAlignAttributeToParent:NSLayoutAttributeLeading constant:0];
+    
+}
+-(NSLayoutConstraint*) constraintAlignTrailingInParent{
+    
+    return [self constraintAlignAttributeToParent:NSLayoutAttributeTrailing constant:0];
+    
+}
+-(NSLayoutConstraint*) constraintAlignTopInParentWithDistance:(CGFloat)distance{
+    
+    return [self constraintAlignAttributeToParent:NSLayoutAttributeTop constant:distance];
+    
+}
+-(NSLayoutConstraint*) constraintAlignBottomInParentWithDistance:(CGFloat)distance{
+    
+    return [self constraintAlignAttributeToParent:NSLayoutAttributeBottom constant:-distance];
+    
+}
+-(NSLayoutConstraint*) constraintAlignLeadingInParentWithDistance:(CGFloat)distance{
+    
+    return [self constraintAlignAttributeToParent:NSLayoutAttributeLeading constant:distance];
+    
+}
+-(NSLayoutConstraint*) constraintAlignTrailingInParentWithDistance:(CGFloat)distance{
+    
+    return [self constraintAlignAttributeToParent:NSLayoutAttributeTrailing constant:-distance];
+    
+}
 
 //-----------------------------------------------------
 // Relative to other views
@@ -254,7 +306,9 @@
 }
 
 - (void)constraintUpdateConstantWidth:(CGFloat)val{
+    
     [self constraintUpdateConstantWidth:val updateLayout:YES];
+    
 }
 
 
