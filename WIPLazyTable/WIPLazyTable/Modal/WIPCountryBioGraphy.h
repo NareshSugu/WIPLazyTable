6d418f88520b19f0321e9c85648bf042fb8c0eb5
Sugu <kumar.sugu.naresh@gmail.com>
2018-05-20 20:14:22 +0530

    [iOS][DATAMODAL] - initial data modal structure created

---
+++ WIPLazyTable/WIPLazyTable/Modal/WIPCountryBioGraphy.h
@@ -0,0 +1,18 @@
+//
+//  WIPCountryBioGraphy.h
+//  WIPLazyTable
+//
+//  Created by Norace Kumar on 5/20/18.
+//  Copyright © 2018 WiproDigital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+@interface WIPCountryBioGraphy : NSObject
+
+@property (nonatomic, strong) NSString  *title;
+@property (nonatomic, strong) UIImage   *tileImage;
+@property (nonatomic, strong) NSString  *descriptionSubTitle;
+
+@end
---
+++ WIPLazyTable/WIPLazyTable/Modal/WIPCountryBioGraphy.m
@@ -0,0 +1,13 @@
+//
+//  WIPCountryBioGraphy.m
+//  WIPLazyTable
+//
+//  Created by Norace Kumar on 5/20/18.
+//  Copyright © 2018 WiproDigital. All rights reserved.
+//
+
+#import "WIPCountryBioGraphy.h"
+
+@implementation WIPCountryBioGraphy
+
+@end
